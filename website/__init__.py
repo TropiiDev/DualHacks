c3eb6ed79e82f6c98a93769de99c7647473ebe73
tropii <97747461+TropiiDev@users.noreply.github.com>
2023-08-03 20:27:03 -0400

    Update init

--- website/__init__.py
+++ website/__init__.py
@@ -6,10 +6,10 @@ def create_app():
     app = Flask(__name__)
 
     from .views import views
-    #from .auth import auth
+    from .auth import auth
 
     # register the blueprints
     app.register_blueprint(views, url_prefix='/')
-    #app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
