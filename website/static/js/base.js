36880c5d037e579554736a19674a5cd0a6f05b00
tropii <97747461+TropiiDev@users.noreply.github.com>
2023-08-03 21:07:51 -0400

    Finished Base Script

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -3,4 +3,21 @@
 let path = window.location.pathname;
 let pagevar = path.split("/");
 
+<<<<<<< Updated upstream
 document.getElementById(pagevar[1]).className = "nav-link active";
+=======
+if (pagevar[1] == "home") {
+    // update the class of the home link to active
+    document.getElementById("home").className = "nav-link active";
+} else if (pagevar[1] == "enroll") {
+    document.getElementById("enroll").className = "nav-link active";
+} else if (pagevar[1] == "login") {
+    document.getElementById("login").className = "nav-link active";
+} else if (pagevar[1] == "pricing") {
+    document.getElementById("pricing").className = "nav-link active";
+} else if (pagevar[1] == "classes") {
+    document.getElementById("classes").className = "nav-link active";
+} else if (pagevar[1] == "courses") {
+    document.getElementById("courses").className = "nav-link active";
+}
+>>>>>>> Stashed changes
