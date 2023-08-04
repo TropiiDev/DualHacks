360773c73e6e69be2712f7b4a39bb21b2ca58c67
JJ Tom <julianjohnt@gmail.com>
2023-08-03 20:12:29 -0500

    lol

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -3,17 +3,6 @@
 let path = window.location.pathname;
 let pagevar = path.split("/");
 
-if (pagevar[1] == "") {
-    // update the class of the home link to active
-    document.getElementById("home").className = "nav-link active";
-} else if (pagevar[1] == "enroll") {
-    document.getElementById("enroll").className = "nav-link active";
-} else if (pagevar[1] == "login") {
-    document.getElementById("login").className = "nav-link active";
-} else if (pagevar[1] == "pricing") {
-    document.getElementById("pricing").className = "nav-link active";
-} else if (pagevar[1] == "classes") {
-    document.getElementById("classes").className = "nav-link active";
-} else if (pagevar[1] == "courses") {
-    document.getElementById("courses").className = "nav-link active";
-}
+// update the class of the home link to active
+document.getElementById(pagevar[1]).className = "nav-link active";
+
