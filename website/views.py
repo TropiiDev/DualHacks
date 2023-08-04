59a48dfae35970131c2b0eac3cb1cef2c34b1a63
JJ Tom <julianjohnt@gmail.com>
2023-08-03 19:15:35 -0500

    added courses

--- website/views.py
+++ website/views.py
@@ -9,3 +9,11 @@ def home():
 @views.route('/pricing')
 def pricing():
     return render_template("pricing.html")
+
+@views.route('/classes')
+def classes():
+    return render_template('classes.html')
+
+@views.route('courses')
+def courses():
+    return render_template('courses.html')
