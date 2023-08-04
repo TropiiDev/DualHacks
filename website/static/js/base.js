5b36d06091e69882d799b8c731bf83f8c8bec0a0
tropii <97747461+TropiiDev@users.noreply.github.com>
2023-08-03 21:14:11 -0400

    update

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -4,5 +4,8 @@ let path = window.location.pathname;
 let pagevar = path.split("/");
 
 // update the class of the home link to active
-document.getElementById(pagevar[1]).className = "nav-link active";
-
+if (pagevar[1] == "") {
+    document.getElementById("home").className = "nav-link active";
+} else {
+    document.getElementById(pagevar[1]).className = "nav-link active";
+}
